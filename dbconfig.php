ff092b237a9c40bb5464996815e826da56d327e9
MdQuais <mdquais8@gmail.com>
2019-10-09 00:08:40 +0530

    first commit

---
+++ dbconfig.php
@@ -0,0 +1,5 @@
+<?php
+$con=mysqli_connect("localhost","root","","discuss");
+//$con=mysqli_connect("localhost","root","saujan","discuss") or die(mysqli_error());
+error_reporting(1);
+?>
---
+++ prog.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			if(j==1||j==n)
+				cout<<1<<" ";
+			else
+				
+		}
+		endl;
+	}
+}
